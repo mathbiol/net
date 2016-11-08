c5d0dbdac117988527cd896c7e8ff67a6c6bb9f2
Jonas Almeida <jalmeida@mathbiol.org>
2016-11-08 09:59:52 -0500

    we have arrows !

--- net.js
+++ net.js
@@ -9,10 +9,10 @@ Net = function(){
     this.edges={}
 }
 
-net=new Net  // default net, will be used if none is declared
+Net.net=new Net  // default net, will be used if none is declared
 
 Node = function(p,nt){ // note second input argument is the net, or nets, this is being registered to
-    nt = nt || net
+    nt = nt || Net.net
     var d = (new Date).toJSON()
     this.id=d+'_Node_'+Math.random().toString().slice(2)
     this.type=this.constructor.name
@@ -31,7 +31,7 @@ Node = function(p,nt){ // note second input argument is the net, or nets, this i
 
 Edge = function(p,nt){
     //if(!nt){nt=[net]}
-    nt = nt || net
+    nt = nt || Net.net
     var d = (new Date).toJSON()
     this.id=d+'_Edge_'+Math.random().toString().slice(2)
     this.type=this.constructor.name
@@ -171,10 +171,117 @@ Net.UI=function(div){
 			})	
 		})
 		// build the plot using http://bl.ocks.org/mbostock/1153292 as a starting point
+		var n3Div = document.createElement('div')
+		div.appendChild(n3Div)
+		div.id='n3Div'
+		Net.d3(n3Div)
+
 		4
 
 
 	}
+}
+
+Net.d3= function(div,net){ // assemble d3 network on div
+	net = net || Net.UI.net
+	// taking clues from http://bl.ocks.org/mbostock/1153292
+	var links = []
+	var types ={}
+	Object.getOwnPropertyNames(net.edges).forEach(function(edid){
+		//console.log(net.edges[edid])
+		var edj=net.edges[edid]
+		var lnk = { // many later enable multiple source and target nodes
+			source:(edj.FROM[0].properties.title || JSON.stringify(edj.FROM[0].properties,null,3)),
+			target:(edj.TO[0].properties.title || JSON.stringify(edj.TO[0].properties,null,3)),
+			type:edj.properties.value||JSON.stringify(edj.properties,null,3)
+		}
+		if(typeof(lnk.type)=='number'){
+			lnk.type=lnk.type.toString()
+		}
+		types[lnk.type]=true
+		links.push(lnk)	
+	})
+	types=Object.getOwnPropertyNames(types)
+	var nodes = {}
+	links.forEach(function(link) {
+  		link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+  		link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+	});
+
+	var width = 300, height = 300;
+
+	var force = d3.layout.force()
+		.nodes(d3.values(nodes))
+		.links(links)
+		.size([width, height])
+		.linkDistance(60)
+		.charge(-300)
+		.on("tick", tick)
+		.start();
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	// Per-type markers, as they don't inherit styles.
+	svg.append("defs").selectAll("marker")
+		//.data(["suit", "licensing", "resolved"])
+		.data(types)
+	  	.enter().append("marker")
+		.attr("id", function(d) { 
+			return d; 
+		 })
+		.attr("viewBox", "0 -5 10 10")
+		.attr("refX", 15)
+		.attr("refY", -1.5)
+		.attr("markerWidth", 6)
+		.attr("markerHeight", 6)
+		.attr("orient", "auto")
+	  	.append("path")
+		.attr("d", "M0,-5L10,0L0,5");
+
+	
+	var path = svg.append("g").selectAll("path")
+		.data(force.links())
+	  	.enter().append("path")
+		.attr("class", function(d) { return "link " + d.type; })
+		.attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+	var circle = svg.append("g").selectAll("circle")
+		.data(force.nodes())
+	  	.enter().append("circle")
+		.attr("r", 6)
+		.call(force.drag);
+
+	var text = svg.append("g").selectAll("text")
+		.data(force.nodes())
+	  	.enter().append("text")
+		.attr("x", 8)
+		.attr("y", ".31em")
+		.text(function(d) { return d.name; });
+
+	// Use elliptical arc path segments to doubly-encode directionality.
+	function tick() {
+	  path.attr("d", linkArc);
+	  circle.attr("transform", transform);
+	  text.attr("transform", transform);
+	}
+
+	function linkArc(d) {
+	  var dx = d.target.x - d.source.x,
+		  dy = d.target.y - d.source.y,
+		  dr = Math.sqrt(dx * dx + dy * dy);
+	  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+	}
+
+	function transform(d) {
+	  return "translate(" + d.x + "," + d.y + ")";
+	}
+
+
+
+	4
+
 }
 
 
